717920e3f41f20def55383ff02b88fa5e9efcbb0
Swapnil Supe <swapnilsupe24@gmail.com>
2025-03-31 23:45:13 +0530

    Your commit message here

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -13,7 +13,7 @@ const HeroPic = () => {
     >
       <img
         src="../../public/images/HexaPic.png"
-        alt="Abdelrahman Qassem"
+        alt="swapnil supe"
         className="max-h-[450px] w-auto"
       />
 
