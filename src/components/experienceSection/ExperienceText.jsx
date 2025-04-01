53e6b673fddd33be4d4eaf348231032f29292b96
Swapnil Supe <swapnilsupe24@gmail.com>
2025-04-01 19:42:36 +0530

    Updated in navbar andexperience change into education

--- src/components/experienceSection/ExperienceText.jsx
+++ src/components/experienceSection/ExperienceText.jsx
@@ -1,7 +1,7 @@
 const ExperienceText = () => {
     return (
       <div className="flex flex-col items-center mt-[100px] ">
-        <h2 className="text-6xl text-cyan mb-10">Experience</h2>
+        <h2 className="text-6xl text-cyan mb-10">Education</h2>
       </div>
     );
   };
--- src/components/navbar/NavbarLinks.jsx
+++ src/components/navbar/NavbarLinks.jsx
@@ -3,7 +3,7 @@ import emailjs from "@emailjs/browser";
 const links = [
   { link: "About Me", section: "about" },
   { link: "Skills", section: "skills" },
-  { link: "Experience", section: "experience" },
+  { link: "Education", section: "experience" },
   { link: "Projects", section: "projects" },
   { link: "Contact", section: "contact" },
 ];
