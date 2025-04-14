9da31f7a52074a1a058660c2a9bfbcbfb960f416
Swapnil Supe <swapnilsupe24@gmail.com>
2025-04-14 17:23:55 +0530

    Certificate sql certificate is add

--- src/components/certificateSection/CertificateSection.jsx
+++ src/components/certificateSection/CertificateSection.jsx
@@ -3,7 +3,7 @@ import CertificateCard from './CertificateCard';
 
 const CertificateSection = () => {
   return (
-    <div id="certificate" className="flex flex-col items-center mt-28 py-10">
+    <div id="certificate" className="flex flex-col items-center mt-40 py-10">
       <h2 className="text-6xl text-cyan mb-10">Certificate</h2>
       <div className="flex gap-10 flex-wrap justify-center">
         <CertificateCard 
@@ -17,6 +17,10 @@ const CertificateSection = () => {
         <CertificateCard 
           title="TCS iON Career Edge - Young Professional" 
           pdf="/certificates/Swapnil_Supe_4639643.pdf" 
+        />
+         <CertificateCard 
+          title="Sql- Island Certifiacte" 
+          pdf="/certificates/Certificate_SQL-Island.pdf" 
         />
       </div>
     </div>
